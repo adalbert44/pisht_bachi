8693ba5305a902a7b96fb12e419ac8a608cebc35
GiraffeHarold <stastomash1@gmail.com>
2016-07-17 21:37:33 +0300

    Update basic_classes.cpp

--- project/basic_classes.cpp
+++ project/basic_classes.cpp
@@ -7,6 +7,7 @@ vector<string> name[200], fort_sprite(200);
 int used_generals[200];
 map <string, string> general_sprite;
 
+/// метод для генерації масиву генералів певної нації
 void init()
 {
 	freopen("generals.dat", "r", stdin);
@@ -26,17 +27,20 @@ void init()
 	}
 }
 
+/// метод для генерації наступного генерала із списку
 string new_name(int color)
 {
 	return name[color][used_generals[color]++];
 }
 
+/// клас міста
 class Town
 {
 public:
 	int x, y, color;
 	Sprite sprite;
-
+	
+	// конструктор міста від координат клітинки та кольору нації
 	Town(int x, int y, int color)
 	{
 	    this->x = x;
@@ -46,6 +50,7 @@ public:
 	}
 };
 
+/// клас генерала
 class General
 {
 public:
@@ -53,6 +58,7 @@ public:
 	string name;
 	Sprite sprite;
 
+	// конструктор від рідного міста генерала
 	General(Town town)
 	{
 		this->x = town.x;
@@ -64,19 +70,23 @@ public:
 	}
 };
 
+/// клас фортеці
 class Fort
 {
 public:
 	int x, y, color;
 	Sprite sprite;
 
+	// конструктор від генерала, який побудував фортецю
 	Fort(General gen)
 	{
 		this->x = gen.x;
 		this->y = gen.y;
 		this->color = gen.color;
 		this->sprite = fort_sprite[this->color];
+		// TODO: знищувати генерала після побудови фортеці
 	}
+	// конструктор від координат клітинки та кольору нації
 	Fort(int x, int y, int color)
 	{
 		this->x = x;
@@ -86,6 +96,7 @@ public:
 	}
 };
 
+/// клас клітинки
 class Cell
 {
 public:
@@ -93,7 +104,9 @@ public:
 	General *general;
 	Fort *fort;
 	Town *town;
-
+	// TODO: тип клітинки: гори, рівнини, моря !!!
+	
+	// автоматичний конструктор пустої клітинки
 	Cell()
 	{
 	    this->color = 0;
