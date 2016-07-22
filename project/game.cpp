0556c7246520f6e21e07828bc5e35e2266474d8e
Pishti <ivan_fekete@i.ua>
2016-07-22 15:38:27 +0300

    додано генератор переходів

--- project/game.cpp
+++ project/game.cpp
@@ -1,19 +1,85 @@
 #include "basic_classes.cpp"
 
+enum players_regime
+{
+    READ_FROM_FILE,
+    PLAYER_PLAYER,
+    PLAYER_COMPUTER
+};
+
 ///pattern game class
+
+
 class FirstRegimeGame
 {
 public:
     players_regime players;
-
+    int N, M;
 	Cell field[100][100];
 
+	vector< pii > turns[100][100];
+
+    ///блок процедур для генерації переходів
+
+    bool inRangeX(int x)
+    {
+        return 1 <= x and x <= N;
+    }
+    bool inRangeY(int y)
+    {
+        return 1 <= y and y <= M;
+    }
+
+    bool inRange(pii u)
+    {
+        return this->inRangeX(u.x) and this->inRangeY(u.y);
+    }
+
+    void addTurn(pii u, pii v)
+    {
+        if(this->inRange(u) && this->inRange(v))
+            turns[u.x][u.y].push_back(v);
+    }
+
+	void generateTurns()
+	{
+        pair<int,int> moves_odd[6] =
+        {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0 , -1},          {0,  1},
+                      { 1, 0}
+        };
+
+        pair<int,int> moves_even[6] =
+        {
+                      {-1, 0},
+            {0 , -1},          {0,  1},
+            {1,  -1}, { 1, 0}, {1,  1}
+        };
+
+        for(int x = 1; x <= this->N; x++)
+            {
+                for(int y = 1; y <= this->M; y++)
+                {
+                    if(y % 2 == 1)
+                    {
+                        for(auto move : moves_odd)
+                            this->addTurn({x, y}, {x + move.x, y + move.y});
+                    }
+                    else
+                    {
+                        for(auto move : moves_even)
+                            this->addTurn({x, y}, {x + move.x, y + move.y});
+                    }
+                }
+            }
+	}
     ///set methods
     void setColorAround(int x, int y, int color)
     {
         this->field[x][y].color = color;
-        for(auto move : moves)
-            this->field[x + move.first][y + move.second].color = color;
+        for(auto turn : turns[x][y])
+            this->field[turn.x][turn.y].color = color;
     }
 
     void setTown(Town *town)
@@ -33,12 +99,16 @@ public:
     ///end set methods
 
     ///constructor
-	FirstRegimeGame(players_regime players, Town *town1, Town *town2)
+	FirstRegimeGame(players_regime players,int width, int height, Town *town1, Town *town2)
 	{
         this->players = players;
 
+        this->N = width;
+        this->M = height;
+
         this->setTown(town1);
         this->setTown(town2);
+        this->generateTurns();
 	}
 
     ///this is empty methods
@@ -77,7 +147,7 @@ int main()
 
 	Town *town1 = new Town(1, 1, 1);
 	Town *town2 = new Town(2, 2, 2);
-	FirstRegimeGame game1 = FirstRegimeGame(READ_FROM_FILE, town1, town2);
+	FirstRegimeGame game1 = FirstRegimeGame(READ_FROM_FILE,10,10, town1, town2);
 	game1.setFort(new Fort(1,2,1));
 	game1.play();
 
