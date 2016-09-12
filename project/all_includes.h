65b21b9a6a9f7556806455529413f1cc4e3e9736
GiraffeHarold <stastomash1@gmail.com>
2016-09-12 18:53:33 +0300

    Update all_includes.h
    додано інклуди для графіки, переведено на GCC.

--- project/all_includes.h
+++ project/all_includes.h
@@ -1,55 +1,45 @@
 /*
 Тут всі інклуди , дефайни і темплейти
 */
+#ifndef MAIN_HEAD_H_INCLUDED
 
-#pragma once
+    #define MAIN_HEAD_H_INCLUDED
 
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <tchar.h>
-#include <stdlib.h>
-#include <vector>
-#include <set>
-#include <queue>
-#include <stack>
-#include <map>
-#include <algorithm>
-#include <random>
-#include <cstdlib>
-#include <time.h>
-#include <ctime>
+    #include <GL/glut.h>
+    #include <IL/ilut.h>
+    #include <bits/stdc++.h>
 
-#define INF 1e9
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define x first
-#define y second
-#define I "%d"
-#define II "%d%d"
-#define III "%d%d%d"
-#define IIII "%d%d%d%d"
+    #define INF 1e9
+    #define pb push_back
+    #define mp make_pair
+    #define fi first
+    #define se second
+    #define x first
+    #define y second
+    #define I "%d"
+    #define II "%d%d"
+    #define III "%d%d%d"
+    #define IIII "%d%d%d%d"
 
-#ifdef _MSC_VER
-	#define _CRT_SECURE_NO_WARNINGS
-#endif
+    using namespace std;
 
-using namespace std;
+    typedef GLuint Sprite;  //GLuint - номер текстури
+    typedef vector<int> vi;
+    typedef pair <int, int> pii;
+    typedef vector<pii> vii;
+    typedef long long ll;
+    typedef unsigned long long ull;
+    typedef long double ld;
+    typedef GLuint Relief;
 
-typedef string Sprite; // TODO: реалізувати класс картинки !!!
-typedef vector<int> vi;
-typedef pair <int, int> pii;
-typedef vector<pii> vii;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
+    const ld PI = acos(-1.0);
 
+    template <typename type> inline type sqr(type x)
+    {
+        return x*x;
+    }
 
-template <typename type> inline type sqr(type x)
-{
-	return x*x;
-}
+#endif // MAIN_HEAD_H_INCLUDED
+
 
 // TODO: reference additional headers your program requires here
